a6895ad3afd1d58f2f8a6408eec78acb8a05748d
刘宇琳 <burnellliu>
2024-12-08 19:09:28 +0800

    测试

--- src/app/memory/page.tsx
+++ src/app/memory/page.tsx
@@ -6,12 +6,13 @@ import { LoadingOutlined } from '@ant-design/icons';
 import { LLM_CONTENT } from '@/app/utils/constant';
 import ReactMarkdown from 'react-markdown';
 import { getStorage, setStorage } from '@/app/utils';
-import OpenAI from 'openai';
+// import OpenAI from 'openai';
 
-const client = new OpenAI({
-  apiKey: 'process.env.moon',
-  baseURL: "https://api.moonshot.cn/v1",
-});
+// const client = () => { }
+// new OpenAI({
+//   apiKey: 'process.env.moon',
+//   baseURL: "https://api.moonshot.cn/v1",
+// });
 
 const Home = () => {
   const [content, setContent] = useState<string>('')
@@ -20,10 +21,10 @@ const Home = () => {
   const [key, setKey] = useState<string>('')
 
   useEffect(() => {
-    const [wordStr, memoryStr] = location.search.replace('?', "").split('&')
+    const [wordStr] = location.search.replace('?', "").split('&')
 
     const word = wordStr.split('=')[1] || ''
-    const memory = memoryStr.split('=')[1] || ''
+    // const memory = memoryStr.split('=')[1] || ''
 
     const dic = getStorage(LLM_CONTENT, {})
 
@@ -33,15 +34,18 @@ const Home = () => {
       setContent(dic[word])
     } else {
       setShow(true)
-      getContent(word, memory)
+      getContent(word)
     }
 
     setKey(process.env.moon || '')
+
+    console.log(process.env.moon);
+
   }, [])
 
-  const getContent = async (word = '', memory = '') => {
+  const getContent = async (word = '') => {
     try {
-      const resJson = await getLLM({ word, memory }) || '';
+      const resJson = '' //await getLLM({ word, memory }) || '';
 
       setContent(resJson)
 
@@ -54,32 +58,32 @@ const Home = () => {
     setShow(false)
   }
 
-  const getLLM = async ({ word = '', memory = '' }) => {
-    try {
-      const completion = await client.chat.completions.create({
-        model: "moonshot-v1-8k",
-        messages: [{
-          role: "system", content: `你是一名英语老师，擅长通过幽默诙谐的方式，让学生记住单词。你擅长将单词拆分，并把拆分的部分画漫画是你的教学方式之一。根据提供的内容，画一幅让学生快速记忆单词的漫画。`
-        }, {
-          role: "user", content: `[单词]="""
-            ${word}
-  """
-  
-  [记忆方式]="""
-  ${memory}
-  """
-  
-  你有充足的时间思考[单词]和[记忆方式]，开始。`
-        }],
-        temperature: 0.3
-      });
-      const content = completion.choices[0].message.content;
+  // const getLLM = async ({ word = '', memory = '' }) => {
+  //   try {
+  //     const completion = await client.chat.completions.create({
+  //       model: "moonshot-v1-8k",
+  //       messages: [{
+  //         role: "system", content: `你是一名英语老师，擅长通过幽默诙谐的方式，让学生记住单词。你擅长将单词拆分，并把拆分的部分画漫画是你的教学方式之一。根据提供的内容，画一幅让学生快速记忆单词的漫画。`
+  //       }, {
+  //         role: "user", content: `[单词]="""
+  //           ${word}
+  // """
 
-      return content;
-    } catch (e) {
-      console.log(e);
-    }
-  }
+  // [记忆方式]="""
+  // ${memory}
+  // """
+
+  // 你有充足的时间思考[单词]和[记忆方式]，开始。`
+  //       }],
+  //       temperature: 0.3
+  //     });
+  //     const content = completion.choices[0].message.content;
+
+  //     return content;
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // }
 
   return (
     <Layout>
